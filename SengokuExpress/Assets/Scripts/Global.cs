d464b02956c6dcaff8f0d0a45c7c651b336f4ead
Matin Kamali <matincode@gmail.com>
2021-10-14 10:08:19 +1100

    added the player developed by matin to prefabs, added reset to PlayerInp

---
+++ SengokuExpress/Assets/Scripts/Global.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Global {
+    // tags
+    public static string tag_environment = "environment";
+    // layers
+}
