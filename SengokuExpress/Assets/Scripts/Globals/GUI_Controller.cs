39a8b1d2f064a7009f77e087f13415214162e5f8
Malloc Studio <46115200+MallocStudio@users.noreply.github.com>
2021-11-03 10:23:00 +1100

    added Text Mesh Pro, working on pause, win, and lost state

---
+++ SengokuExpress/Assets/Scripts/Globals/GUI_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUI_Controller : MonoBehaviour {
+    public enum PANELS {
+        LOST, WIN, GAME,
+    }
+    // !== public delegates the Unity Editor Buttons can use ==! //
+    public int mainMenuSceneIndex = 0;
+    public RectTransform lostPanel = null;
+    public RectTransform winPanel = null;
+    public RectTransform gamePanel = null;
+    /// restart level
+    public void restart_level() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        switch_panel(PANELS.GAME);
+    }
+    /// go to main menu
+    public void go_to_main_menu() {
+        SceneManager.LoadScene(mainMenuSceneIndex);
+    }
+    /// resume the game if paused
+    public void resume_game() {
+        Global.set_pause(false);
+        switch_panel(PANELS.GAME);
+    }
+
+    // !== public methods not meant to be used by the Unity Editor ==! //
+    public void switch_panel(PANELS panel) {
+        switch (panel) {
+            case PANELS.LOST: {
+                if (lostPanel != null) lostPanel.gameObject.SetActive(true);
+                if (winPanel  != null) lostPanel.gameObject.SetActive(false);
+                if (gamePanel != null) lostPanel.gameObject.SetActive(false);
+            } break;
+            case PANELS.WIN: {
+                if (lostPanel != null) lostPanel.gameObject.SetActive(false);
+                if (winPanel  != null) lostPanel.gameObject.SetActive(true);
+                if (gamePanel != null) lostPanel.gameObject.SetActive(false);
+            } break;
+            case PANELS.GAME: {
+                if (lostPanel != null) lostPanel.gameObject.SetActive(false);
+                if (winPanel  != null) lostPanel.gameObject.SetActive(false);
+                if (gamePanel != null) lostPanel.gameObject.SetActive(true);
+            } break;
+        }
+    }
+}
--- SengokuExpress/Assets/Scripts/Globals/Global.cs
+++ SengokuExpress/Assets/Scripts/Globals/Global.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class Global {
-    // tags
+    /// tags
     public static string tag_environment = "environment";
-    // layers
+    /// layers
+    /// pause system
+    private static float pre_pause_time_scale = 1; // ! used to reset time scale to what it was before pausing the game, in case we changed it for slow mo effects
+    public static void set_pause(bool value) {
+        if (value) {
+            pre_pause_time_scale = Time.timeScale;
+            Time.timeScale = 0;
+        }
+        else Time.timeScale = pre_pause_time_scale;
+    }
 }
