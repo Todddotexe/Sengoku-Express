ab7c6921d47086ff147c795e91ac4c002e47362b
Malloc Studio <46115200+MallocStudio@users.noreply.github.com>
2021-11-29 14:32:45 +1100

    added Particle Player to be added on any environmental prefab

---
+++ SengokuExpress/Assets/Scripts/Utilities/Particle_Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Particle_Player : MonoBehaviour {
+    public ParticleSystem particle = null;
+    
+    void OnTriggerEnter(Collider collider) {
+        if (particle != null && collider.tag == Global.tag_player) {
+            particle.Play();
+        }
+    }
+}
