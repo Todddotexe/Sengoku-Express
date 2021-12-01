5d62c3df4f5facf43f7e82bc0259445e1413ee7f
ad3nardi <ad3nardi@gmail.com>
2021-12-01 11:08:05 +1100

    finished credits yay

--- SengokuExpress/Assets/Scripts/Utilities/End_BBox.cs
+++ SengokuExpress/Assets/Scripts/Utilities/End_BBox.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class End_BBox : MonoBehaviour {
 
     void OnTriggerEnter(Collider col) {
-        if (col.tag == Global.tag_player) {
+        
             Global.set_game_state(Global.STATES.WIN);
-            Destroy(gameObject);
-        }
+            //Destroy(gameObject);
+        
     }    
 }
